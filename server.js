14b8ddae88524a33d1c3147f1e724a1b6ad8d2d2
shnorlaxzz <mshnas56@gmail.com>
2023-02-26 20:47:37 +0000

    lab 12

--- server.js
+++ server.js
@@ -9,20 +9,31 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 const PORT = 3000;
-server.get('/', (req, res) => {
-    const movieData=require('./Movie Data/data.json')
-    const spiderMan=new Movie(movieData.title,movieData.poster_path,movieData.overview);
-    res.send(spiderMan);
-})
-server.get('/favorite', (req, res) => {
+
+// Routes
+server.get('/', homeHandler);
+server.get('/favorite', favoriteHandler)
+server.get('error', serverErrorHandler)
+server.get('*',defaultHandler)
+
+// Handlers functions
+function homeHandler(req,res)
+{
+        const movieData=require('./Movie Data/data.json')
+        const spiderMan=new Movie(movieData.title,movieData.poster_path,movieData.overview);
+        res.send(spiderMan);
+}
+function favoriteHandler (req, res)  {
     res.send("Welcome to favorite page");
-})
-server.get('*', (req, res) => {
-    res.status(404).send("page not found");
-})
-server.get('error', (req, res) => {
+}
+function serverErrorHandler(req, res){
     res.status(500).send("internal server error");
-})
+}
+function defaultHandler(req, res) {
+    res.status(404).send("page not found");
+}
+
+//http://localhost:3000/
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
