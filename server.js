0cd0df065a0b66175a1155d7ade2d13f30dc415d
shnorlaxzz <mshnas56@gmail.com>
2023-03-06 07:12:16 +0000

    response fixed

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ function searchHandler(req, res) {
         const query_value = "man";
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query_value}&page=2`
         axios.get(url)
-            .then((result) => { return result.data })
+            .then((result) => { res.send(result.data) })
             .catch((err) => {
                 console.log("sorry", err);
                 res.status(500).send(err);
@@ -88,7 +88,7 @@ function genreHandler(req, res) {
         const api_key = process.env.api_key;
         const url=`https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}&language=en-US`
         axios.get(url)
-        .then((result)=>{return result.data})
+        .then((result)=>{res.send(result.data)})
         .catch((err) => {
             console.log("sorry", err);
             res.status(500).send(err);
@@ -106,7 +106,7 @@ function personHandler(req, res) {
         const id=4;
         const url=`https://api.themoviedb.org/3/person/${id}?api_key=${api_key}&language=en-US`
         axios.get(url)
-        .then((result)=>{return result.data})
+        .then((result)=>{res.send(result.data)})
         .catch((err) => {
             console.log("sorry", err);
             res.status(500).send(err);
