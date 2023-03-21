c8edc0f87666b818346b8da5a201fcf4dba801d4
shnorlaxzz <mshnas56@gmail.com>
2023-03-21 13:47:40 +0000

    updated schema and post query

--- schema.sql
+++ schema.sql
@@ -2,5 +2,8 @@ DROP TABLE IF EXISTS new_movies;
 CREATE TABLE IF NOT EXISTS new_movies (
 movie_id SERIAL PRIMARY KEY,
 movie_name TEXT NOT NULL,
-movie_comments TEXT NOT NULL
+movie_comments TEXT, 
+movie_overview TEXT ,
+poster_path TEXT ,
+release_date VARCHAR (300)
 );
